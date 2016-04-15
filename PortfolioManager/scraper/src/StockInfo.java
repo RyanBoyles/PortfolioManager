e1b8dd5ea42db52dbb957655fc33f119f1c8d9cc
Jeff L <j.lu368@gmail.com>
2016-04-15 11:07:49 -0500

    Added functionality for scraper
    Added more attributes, and scrapeAll returns StockInfo object.

--- PortfolioManager/scraper/src/StockInfo.java
+++ PortfolioManager/scraper/src/StockInfo.java
@@ -5,10 +5,36 @@ public class StockInfo {
     double price;
     double priceChange;
     double perChange;
+    double open;
+    double todayHigh;
+    double todayLow;
+    double weekHigh;
+    double weekLow;
+    double pe;
+    double yield;
 
-    StockInfo(double price, double priceChange, double perChange){
+
+    public StockInfo(){
+        price = perChange = priceChange = open = todayHigh = todayLow = weekLow = weekHigh = pe = yield = 0;
+    }
+
+    public StockInfo(double price, double priceChange, double perChange){
         this.price = price;
         this.priceChange = priceChange;
         this.perChange = perChange;
+    }
+
+    public StockInfo(double price, double priceChange, double perChange, double open, double todayHigh,
+                     double todayLow, double weekHigh, double weekLow, double pe, double yield){
+        this.price = price;
+        this.priceChange = priceChange;
+        this.perChange = perChange;
+        this.open = open;
+        this.todayHigh = todayHigh;
+        this.todayLow = todayLow;
+        this.weekHigh = weekHigh;
+        this.weekLow = weekLow;
+        this.pe = pe;
+        this.yield = yield;
     }
 }
